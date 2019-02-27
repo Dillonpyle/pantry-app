9be146ae379408e6ec0889b3f5eadc1ae20ef1cb
Sam Brown <stbrown463@gmail.com>
2019-02-26 18:16:16 -0600

    show update and delete routes for ingredients created

--- resources/ingredients.py
+++ resources/ingredients.py
@@ -11,6 +11,15 @@ ingredient_fields = {
 	'type': fields.String
 }
 
+def ingredient_or_404(ingredient_id):
+	try:
+		ingredient = models.Ingredient.get(models.Ingredient.id == ingredient_id)
+	except models.Ingredient.DoesNotExist:
+		## this sends our 404 response for us
+		abort(404)
+	else: 
+		return ingredient
+
 class IngredientList(Resource):
 	def __init__(self):
 		self.reqparse = reqparse.RequestParser()
@@ -41,6 +50,45 @@ class IngredientList(Resource):
 		ingredient = models.Ingredient.create(**args)
 		return ingredient
 
+class Ingredient(Resource):
+	def __init__(self):
+		self.reqparse = reqparse.RequestParser()
+		self.reqparse.add_argument(
+			'name',
+			required = False,
+			help = "No ingredient name provided",
+			location = ['form', 'json']
+			)
+		self.reqparse.add_argument(
+			'type',
+			required = False,
+			help = "No ingredient type provided",
+			location = ['form', 'json']
+			)
+
+	## Show route -- working
+	@marshal_with(ingredient_fields)
+	def get(self, id):
+		return ingredient_or_404(id)
+    ## define a function to find our ingredient or send our 404		
+
+  ## Update route -- NOT working
+	@marshal_with(ingredient_fields)
+	def put(self, id):
+		args = self.reqparse.parse_args()
+		query = models.Ingredient.update(**args).where(models.Ingredient.id==id)
+		query.execute()
+		return (models.Ingredient.get(models.Ingredient.id==id), 200)
+
+	## delete route -- working
+	def delete(self, id):
+		query = models.Ingredient.delete().where(models.Ingredient.id==id)
+		query.execute()
+		return "Ingredient was deleted"
+
+
+
+
 ingredients_api = Blueprint('resources.ingredients', __name__)
 api = Api(ingredients_api)
 
@@ -50,6 +98,11 @@ api.add_resource(
 	endpoint="ingredients"
 	)
 
+api.add_resource(
+	Ingredient,
+	'/ingredients/<int:id>',
+	endpoint='ingredient'
+	)
 
 
 
