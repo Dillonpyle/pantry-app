ae4396b3261efb898ef4d6d633b7d224dff1bdb6
Computer <computer@Computers-MacBook-Pro.local>
2019-02-27 08:26:21 -0700

    made login, register, and logout routes

--- app.py
+++ app.py
@@ -1,27 +1,88 @@
-from flask import Flask
+from flask import Flask, g, render_template, flash, redirect, url_for
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from flask_bcrypt import check_password_hash
 from flask_cors import CORS
 
-## Import resources
 from resources.ingredients import ingredients_api
 
-## Import Models
+
 import models
+import forms
+
 
 DEBUG = True
 PORT = 8000
 
 app = Flask(__name__)
+app.secret_key = 'adkjfalj.adflja.dfnasdf.asd'
 
-## set up cors
-CORS(ingredients_api, origin=["http://localhost:3000"], supports_credentials=True)
+login_manager = LoginManager()
+# sets up our login for the app
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(userid):
+    try:
+        return models.User.get(models.User.id == userid)
+    except models.DoesNotExist:
+        return None
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = forms.RegisterForm()
+    if form.validate_on_submit():
+        flash('Yay you registered', 'success')
+        models.User.create_user(
+            username=form.username.data,
+            password=form.password.data
+        )
+
+        return redirect(url_for('index'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    form = forms.LoginForm()
+    if form.validate_on_submit():
+        try:
+            user = models.User.get(models.User.username == form.username.data)
+        except models.DoesNotExist:
+            flash("your email or password doesn't match", "error")
+        else:
+            if check_password_hash(user.password, form.password.data):
+                # creates session
+                login_user(user)
+                flash("You've been logged in", "success")
+                return redirect(url_for('index'))
+            else:
+                flash("your email or password doesn't match", "error")
+    return render_template('login.html', form=form)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash("You've been logged out", "success")
+    return redirect(url_for('index'))
+
+
+# set up cors
+CORS(ingredients_api, origin=[
+     "http://localhost:3000"], supports_credentials=True)
 
 app.register_blueprint(ingredients_api, url_prefix='/api/v1')
 
 
 @app.route('/')
 def hello_world():
-	return 'Pantry App'
+    return 'Pantry App'
+
 
 if __name__ == '__main__':
-	models.initialize()
-	app.run(debug=DEBUG, port=PORT)
+    models.initialize()
+    app.run(debug=DEBUG, port=PORT)
--- models.py
+++ models.py
@@ -5,73 +5,81 @@ from flask_login import UserMixin
 
 DATABASE = SqliteDatabase('mypantry.sqlite')
 
+
 class User(UserMixin, Model):
-	username: CharField(unique=True)
-	password: CharField()
-	photo: CharField()
+    username: CharField(unique=True)
+    password: CharField()
+    photo: CharField()
 
-	class Meta:
-		database = DATABASE
+    class Meta:
+        database = DATABASE
 
-	@classmethod
-	def create_user(cls, username, password, photo):
-		try:
-			cls.select().where(
-				(cls.username==username)
-				).get()
-		except cls.DoesNotExist:
-			user = cls(username=username)
-			user.password = generate_password_hash(password)
-			user.save()
-			return user
-		else:
-			raise Exception("User with email or username already exists")
+    @classmethod
+    def create_user(cls, username, password, photo):
+        try:
+            cls.select().where(
+                (cls.username == username)
+            ).get()
+        except cls.DoesNotExist:
+            user = cls(username=username)
+            user.password = generate_password_hash(password)
+            user.save()
+            return user
+        else:
+            raise Exception("User with email or username already exists")
 
 
 class Ingredient(Model):
-	name = CharField()
-	type = CharField()
+    name = CharField()
+    type = CharField()
 
-	class Meta:
-		database = DATABASE
+    class Meta:
+        database = DATABASE
+
 
 class Recipe(Model):
-	title: CharField()
-	image_url: CharField()
-	source_url: CharField()
-	publisher: CharField()
-	publisher_url: CharField()
-	social_rank: CharField()
-	created_by: ForeignKeyField(User)
+    title: CharField()
+    image_url: CharField()
+    source_url: CharField()
+    publisher: CharField()
+    publisher_url: CharField()
+    social_rank: CharField()
+    created_by: ForeignKeyField(User)
 
-	class Meta: 
-		database = DATABASE
+    class Meta:
+        database = DATABASE
 
-## Ingredient of User 
-class Pantry(Model): 
-	ingedient_id: ForeignKeyField(Ingredient)
-	user_id: ForeignKeyField(User)
-	quantity: IntegerField()
-	created_at = DateTimeField(default=datetime.datetime.now)
-	class Meta: 
-		database = DATABASE
+# Ingredient of User
+
+
+class Pantry(Model):
+    ingedient_id: ForeignKeyField(Ingredient)
+    user_id: ForeignKeyField(User)
+    quantity: IntegerField()
+    created_at = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        database = DATABASE
+
 
 class IngredientInRecipe(Model):
-		recipe_id: ForeignKeyField(Recipe)
-		ingredient_id: ForeignKeyField(Ingredient)
+    recipe_id: ForeignKeyField(Recipe)
+    ingredient_id: ForeignKeyField(Ingredient)
 
-		class Meta: 
-			database = DATABASE
+    class Meta:
+        database = DATABASE
+
 
 class RecipeOfUser(Model):
-	user_id: ForeignKeyField(User)
-	recipe_id: ForeignKeyField(Recipe)
+    user_id: ForeignKeyField(User)
+    recipe_id: ForeignKeyField(Recipe)
 
-	class Meta: 
-		database = DATABASE
+    class Meta:
+        database = DATABASE
 
 
 def initialize():
-	DATABASE.connect()
-	DATABASE.create_tables([User, Ingredient, Recipe, Pantry, IngredientInRecipe, RecipeOfUser], safe=True)
-	DATABASE.close()
+    DATABASE.connect()
+    DATABASE.create_tables(
+        [User, Ingredient, Recipe, Pantry, IngredientInRecipe, RecipeOfUser], safe=True)
+    DATABASE.close()
