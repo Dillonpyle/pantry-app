b2e034b7e297be5d0ebb18080f8a7ccc9208ebe1
Computer <computer@Computers-MacBook-Pro.local>
2019-02-28 09:30:30 -0700

    passed users id

--- app.py
+++ app.py
@@ -31,47 +31,6 @@ def load_user(userid):
         return None
 
 
-@app.route('/register', methods=('GET', 'POST'))
-def register():
-    form = forms.RegisterForm()
-    if form.validate_on_submit():
-        flash('Yay you registered', 'success')
-        models.User.create_user(
-            username=form.username.data,
-            password=form.password.data
-        )
-
-        return redirect(url_for('index'))
-    return render_template('register.html', form=form)
-
-
-@app.route('/login', methods=('GET', 'POST'))
-def login():
-    form = forms.LoginForm()
-    if form.validate_on_submit():
-        try:
-            user = models.User.get(models.User.username == form.username.data)
-        except models.DoesNotExist:
-            flash("your email or password doesn't match", "error")
-        else:
-            if check_password_hash(user.password, form.password.data):
-                # creates session
-                login_user(user)
-                flash("You've been logged in", "success")
-                return redirect(url_for('index'))
-            else:
-                flash("your email or password doesn't match", "error")
-    return render_template('login.html', form=form)
-
-
-@app.route('/logout')
-@login_required
-def logout():
-    logout_user()
-    flash("You've been logged out", "success")
-    return redirect(url_for('index'))
-
-
 # set up cors
 CORS(ingredients_api, origins=[
      "http://localhost:3000"], supports_credentials=True)
