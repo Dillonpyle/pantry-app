4c8eb3d536ceeb14f5341911ed4eee08e23df1bb
Sam Brown <stbrown463@gmail.com>
2019-03-01 17:30:07 -0600

    preliminary ir post created, ir delete logic typed out

--- resources/ingredient_in_recipe.py
+++ resources/ingredient_in_recipe.py
@@ -50,54 +50,65 @@ class RecipeIngredient(Resource):
 			help = "no user_id provided",
 			location = ['form', 'json']
 			)
+		# super().__init__()
 
 # # Add ingredient to recipe
-# # need user id and recipe id
+# # need user id and recipe id ### NEEDS USER AUTH
+	@marshal_with(ingredient_in_recipe_fields)
 	def post(self, r_id, i_id):
 		args = self.reqparse.parse_args()
-		print(args, 'hitting args in post request in dog api')
+		print(args, 'hitting args in post route in ingedient_in_recipe')
+		try:
+			## check db to see if ingredientInRecipe already exists
+			ing_in_recipe = models.IngredientInRecipe.get(models.IngredientInRecipe.recipe_id == r_id and models.IngredientInRecipe.ingredient_id == i_id )
+			return ing_in_recipe
 
-		return 'hey there pal'
-
-
-	# ## create new pantry entry -- Working
-	# @marshal_with(pantry_fields)
-	# def post(self):
-	# 	args = self.reqparse.parse_args()
-	# 	print(args, '-- args in post request in pantry api')
-	# 	## check db to see if pantry item with user_id and ingredient_id already exists
-	# 	try:
-
-	# 		## find pantry entry matching user_id and ingredient_id
-	# 		pantry_entry = models.Pantry.get(models.Pantry.user_id == args.user_id and models.Pantry.ingredient_id == args.ingredient_id )
-	# 		print(pantry_entry.__dict__, 'this is pantry_entry')
-			
-	# 		## increase pantry_entry quantity by 1
-	# 		pantry_entry.quantity += 1
-	# 		pantry_entry.save()
-	# 		print(pantry_entry.__dict__, 'this is pantry_entry with increased quantity')
-	# 		return pantry_entry
-
-	# 	## if it doesn't create pantry item
-	# 	except models.Pantry.DoesNotExist:
-	# 		print(args, '-- args after quantity has been defined')
-	# 		print(args["ingredient_id"], 'this is ingredient id')
-	# 		pantry_entry = models.Pantry.create(
-	# 			ingredient_id=args["ingredient_id"],
-	# 			user_id=args["user_id"],
-	# 			quantity=1
-	# 		)
-	# 		pantry_entry.save()
-	# 		print(pantry_entry.__dict__)
-	# 		return pantry_entry
+		except models.IngredientInRecipe.DoesNotExist:
+			ing_in_recipe = models.IngredientInRecipe.create(
+				recipe_id=r_id,
+				ingredient_id=i_id,
+				amount=args["amount"],
+				unit=args["unit"]
+			)
+			ing_in_recipe.save()
+			print(ing_in_recipe, 'this is ing_in_recipe that was created')
+			return ing_in_recipe
 
 
 
-# # Remove ingredient from recipe
-# # need user id and recipe id
+class RecipeIngredientEdit(Resource):
+	def __init__(self):
+		self.reqparse = reqparse.RequestParser()
+		self.reqparse.add_argument(
+			'amount',
+			required = True,
+			help = "no user_id provided",
+			location = ['form', 'json']
+			)
+		self.reqparse.add_argument(
+			'unit',
+			required = True,
+			help = "no user_id provided",
+			location = ['form', 'json']
+			)
+
+	# # Remove ingredient from recipe
+	# # need user id and recipe id
+	# def delete(self, r_id, i_id, u_id):
+		# check if user created recipe
+		# select r of r_id where created_by == u_id
+		## if user did create recipe
+		### delete ri where r_id == r_id and i_id == i_id
+		## if user didn't create recipe tell them to fuck off
+
+
+
 
 # # Edit amount or unit of ingredients in recipe
 # # need user id and recipe id
+
+
+
 
 
 ingredient_in_recipe_api = Blueprint('resources.ingredient_in_recipe', __name__)
@@ -105,7 +116,7 @@ api = Api(ingredient_in_recipe_api)
 
 api.add_resource(
 	RecipeIngredientList,
-	'/recipe_ingredient_list/<int:r_id>',
+	'/recipe_ingredient/list/<int:r_id>',
 	endpoint="recipe_ingredient_list"
 	)
 
@@ -115,5 +126,9 @@ api.add_resource(
 	endpoint="recipe_ingredient"
 	)
 
-
+api.add_resource(
+	RecipeIngredient,
+	'/recipe_ingredient/edit/<int:r_id>/<int:i_id>/<int:u_id>',
+	endpoint="recipe_ingredient_edit"
+	)
 
