81ce4161928fc214772e5c05c9f13743eede883b
Sam Brown <stbrown463@gmail.com>
2019-03-02 11:05:29 -0600

    ingredient_in_recipe get returning all ingredients of recipe when sent recipe_id

--- resources/ingredient_in_recipe.py
+++ resources/ingredient_in_recipe.py
@@ -13,24 +13,87 @@ ingredient_in_recipe_fields = {
 	'unit': fields.String
 }
 
+# >>> query = User.select()
+# >>> [user.username for user in query]
+# ['Charlie', 'Huey', 'Peewee']
+
+# >>> query[1]
+# <__main__.User at 0x7f83e80f5550>
+
+# >>> query[1].username
+# 'Huey'
+
+# >>> query[:2]
+# [<__main__.User at 0x7f83e80f53a8>, <__main__.User at 0x7f83e80f5550>]
+
+#### NONE OF THIS BULLSHIT WORKS ============================================
+
+# class AllIngredientsInRecipes(Resource):
+# 	def get(self):
+# 		try:
+# 			query = models.IngredientInRecipe.selcet()
+# 			ingredient_in_recipe = [ingredient for ingredient in query]
 
 
 
-# Show all ingredients in recipe -- UNTESTED, add some ingredients to a recipe
-# need recipe id
+# # Show all ingredients in recipe -- UNTESTED, add some ingredients to a recipe
+# # need recipe id
+# class RecipeIngredientList(Resource):
+# 	def get(self, r_id):
+# 		try:
+# 			## query is not working... make it work
+# 			query = models.IngredientInRecipe.select().where(models.IngredientInRecipe.recipe_id == r_id)
+# 			recipe_ingredinets = [ingredient.unit for ingredient in query]
+
+# 			# recipe_ingredinets = [marshal(list_item, ingredient_in_recipe_fields) for list_item in models.IngredientInRecipe.select().where(models.IngredientInRecipe.recipe_id == r_id)]
+
+# 			print(recipe_ingredients)
+# 			return recipe_ingredients
+
+
+# 			## MIGHT STILL NEED THIS
+# 			# recipe_ingredients = models.IngredientInRecipe.get(models.IngredientInRecipe.id == id)
+# 			# return marshal(recipe_ingredients, ingredient_in_recipe_fields)
+# 		except models.IngredientInRecipe.DoesNotExist:
+# 			return 'ingredients in recipe do not exist does not exist'
+
+# 		# dogs = [marshal(dog, dog_fields) for dog in models.Dog.select()]
+
+## function that finds ingredients of recipe
+def i_in_r_or_404_id(recipe_id):
+	try:
+		ingredients = models.IngredientInRecipe.select().where(models.IngredientInRecipe.recipe_id == recipe_id)
+		return ingredients
+	except models.IngredientInRecipe.DoesNotExist:
+		## this sends our 404 response for us
+		abort(404)
+
 class RecipeIngredientList(Resource):
-	def get(self, r_id):
+	def __init__(self):
+		self.reqparse = reqparse.RequestParser()
+		self.reqparse.add_argument(
+			'recipe_id',
+			required = True,
+			help = "no user_id provided",
+			location = ['form', 'json']
+			)
+
+	## Display all Ingredients In Recipe ============================= NOT WORKING
+	# @marshal_with(pantry_fields)
+	def post(self):
 		try:
-			recipe_ingredinets = [marshal(list_item, ingredient_in_recipe_fields) for list_item in models.IngredientInRecipe.select().where(models.IngredientInRecipe.recipe_id == r_id)]
-			return recipe_ingredients
+			args = self.reqparse.parse_args()
+			print(args, 'these are args')
+			print(args.recipe_id, 'this is args.recipe_id')
+			ingredients_in_recipe = i_in_r_or_404_id(args.recipe_id)
+			return [marshal(ingredient, ingredient_in_recipe_fields) for ingredient in ingredients_in_recipe]
+			# return "it'll be ok"
+		except Exception as e:
+			print(e)
+			return "there was an error"
 
-			## MIGHT STILL NEED THIS
-			# recipe_ingredients = models.IngredientInRecipe.get(models.IngredientInRecipe.id == id)
-			# return marshal(recipe_ingredients, ingredient_in_recipe_fields)
-		except models.IngredientInRecipe.DoesNotExist:
-			return 'ingredients in recipe do not exist does not exist'
 
-		# dogs = [marshal(dog, dog_fields) for dog in models.Dog.select()]
+## ===================================================================
 
 
 
@@ -54,6 +117,7 @@ class RecipeIngredient(Resource):
 
 # # Add ingredient to recipe
 # # need user id and recipe id ### NEEDS USER AUTH
+## rewrite to inclue u_id
 	@marshal_with(ingredient_in_recipe_fields)
 	def post(self, r_id, i_id):
 		args = self.reqparse.parse_args()
@@ -94,12 +158,19 @@ class RecipeIngredientEdit(Resource):
 
 	# # Remove ingredient from recipe
 	# # need user id and recipe id
-	# def delete(self, r_id, i_id, u_id):
-		# check if user created recipe
-		# select r of r_id where created_by == u_id
-		## if user did create recipe
-		### delete ri where r_id == r_id and i_id == i_id
-		## if user didn't create recipe tell them to fuck off
+	def delete(self, r_id, i_id, u_id):
+		try:
+			# check if user created recipe
+			models.Recipe.select().where(models.Recipe.created_by == u_id and models.Recipe.id == r_id)
+			## if user did create recipe
+			### delete ri where r_id == r_id and i_id == i_id
+			query = models.IngredientInRecipe.delete().where(models.IngredientInRecipe.recipe_id == r_id and models.IngredientInRecipe.ingredient_id == i_id)
+			query.execute()
+			return "ingredient in recipe deleted"
+		except models.Recipe.DoesNotExist:
+			## if user didn't create recipe tell them to fuck off
+			return "user does not have permission or recipe does not exist"
+
 
 
 
@@ -116,7 +187,7 @@ api = Api(ingredient_in_recipe_api)
 
 api.add_resource(
 	RecipeIngredientList,
-	'/recipe_ingredient/list/<int:r_id>',
+	'/recipe_ingredient/list',
 	endpoint="recipe_ingredient_list"
 	)
 
